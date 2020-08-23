9b813a820982c750ccb1697c3b7eb008bbc78fd5
devesh760 <60389654+devesh760@users.noreply.github.com>
2020-08-23 23:00:10 +0530

    Add files via upload

---
+++ Problems/PrintAllPermutation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int printAllPermutation(string str, int i, int n)
+{
+    static int c;
+    if (i == n)
+    {
+        c++;
+        cout << str << ' ';
+        // return c;
+    }
+    else
+    {
+        for (int j = i; j < n; ++j)
+        {
+            swap(str[i], str[j]);
+            printAllPermutation(str, i + 1, n);
+        }
+    }
+    return c;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    cout << printAllPermutation(str, 0, str.length());
+}
